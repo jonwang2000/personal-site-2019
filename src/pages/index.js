692ecbba4f11e5fe1851f043a1db510e781149d6
Jonathan Wang <j2279wan@edu.uwaterloo.ca>
2019-08-03 00:46:49 -0400

    Just some really basic stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => {
+    return (
+        <div>
+            <h1>Jonathan Wang</h1>
+            <h2>Mathematics, UW 2023</h2>
+
+            <p>I'm a mathematics student at the University of Waterloo, working on my programming and math skills.
+                I'm always looking to learn new things, and I enjoy hands-on experiences.</p>
+            <p>I'm a fan of video games, cooking, and the Toronto Raptors.</p>
+        </div>
+    )
+}
