fdc9fe5c6ee0c2f50b01ce83086829ee22bb90cd
jonwang2000 <j2279wan@edu.uwaterloo.ca>
2019-08-04 19:47:38 -0400

    Typography installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@
  */
 
 module.exports = {
-    plugins: [`gatsby-plugin-styled-components`],
+    plugins: [
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+    ],
 }
